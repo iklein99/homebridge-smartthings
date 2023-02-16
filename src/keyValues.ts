027272ea3c803d85d51e4fe3fbcc58fd2cf159dd
iklein99 <iklein99@comcast.net>
2023-02-16 13:51:53 -0500

    Env var for webhook url for testing

--- src/keyValues.ts
+++ src/keyValues.ts
@@ -1,4 +1,4 @@
-export const WEBHOOK_URL = 'https://stwh.kleinstudios.net/api/';
+export const WEBHOOK_URL = process.env.WEBHOOK_URL ? process.env.WEBHOOK_URL : 'https://stwh.kleinstudios.net/api/';
 export const WH_CONNECT_RETRY_MINUTES = 1;
 
 export async function wait(seconds):Promise<void> {
